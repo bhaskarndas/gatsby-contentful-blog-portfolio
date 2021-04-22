eb51e7babe190d90d1706666bd0745cbbd52856a
Bhaskar Narayan Das <bhaskarndas@gmail.com>
2021-04-22 22:37:08 +0530

    Update MapContact.js

--- src/components/Contact/MapContact.js
+++ src/components/Contact/MapContact.js
@@ -16,7 +16,7 @@ const MapContact = () => {
           scrolling="no"
           marginHeight="0"
           marginWidth="0"
-          src="https://maps.google.com/maps?q=Via%20oriani%2022%20Cervia&amp;t=m&amp;z=15&amp;output=embed&amp;iwloc=near"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3451.6143304612497!2d78.29068951441839!3d30.105229881860197!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39093df8048505d5%3A0xade4df44835dacb4!2sSanrachna%20Kriti%20(Ar.%20Shobha%20Rajvanshi)!5e0!3m2!1sen!2sin!4v1618980292135!5m2!1sen!2sin"
           aria-label="Via oriani 22 Cervia"
         ></iframe>
       </MapWrapper>
